e67d35c5cf0733298753e143f5abd4812df562d8
jarelken <lelo_taguti@hotmail.com>
2021-01-27 19:53:07 -0300

    Final da primeira aula

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
-import { urlObjectKeys } from 'next/dist/next-server/lib/utils';
 import styled from 'styled-components'
 import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
 
-
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center:
-`;
+//Substituído por QuizBackground
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center:
+// `;
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -21,13 +24,30 @@ export const QuizContainer = styled.div`
   }
 `;
 
+
 export default function Home() {
   return(
-    <BackgroundImage>
+    <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-        asdsad
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz sem nome</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Por enquanto é só isso.</p>
+          </Widget.Content>
+        </Widget>
+          
+        <Widget>
+          <Widget.Content>
+            <h1>Quiz sem nome</h1>
+            <p>Por enquanto é só isso.</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
       </QuizContainer>
-    </BackgroundImage>
+      <GitHubCorner projectUrl="https://github.com/jarelken"/>
+    </QuizBackground>
 
   ) 
 }
